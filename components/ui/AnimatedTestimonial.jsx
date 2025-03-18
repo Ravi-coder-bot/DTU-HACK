1eef6f409612a35be112f647aa24bde954ba73bd
ravinder <ravibeniwal931@gmail.com>
2025-03-18 12:24:07 +0530

    testimonial-color change

--- components/ui/AnimatedTestimonial.jsx
+++ components/ui/AnimatedTestimonial.jsx
@@ -100,7 +100,7 @@ export const AnimatedTestimonials = ({
               duration: 0.2,
               ease: "easeInOut",
             }}>
-            <h3 className="text-2xl font-bold text-black dark:text-white">
+            <h3 className="text-2xl font-bold text-blue-500 dark:text-white">
               {testimonials[active].name}
             </h3>
             <p className="text-sm text-gray-500 dark:text-neutral-500">
